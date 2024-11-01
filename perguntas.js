7559c5a1fb43f8c31c7f1b3adbd53e5551ab06b2
juliakuhnestudante <108340508+juliakuhnestudante@users.noreply.github.com>
2024-11-01 16:02:47 -0300

    01/11/2024

---
+++ perguntas.js
@@ -0,0 +1,107 @@
+export const perguntas = [
+    {
+        enunciado: "Você se considera fisicamente ativo? ",
+        alternativas: [
+            {
+                texto: "Sim, pratico atividades físicas regularmente.",
+                afirmacao: [
+                    "Acredito que a atividade física é essencial para a saúde.",
+                    "Sinto que me ajuda a ter mais energia e disposição."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Não, não pratico atividades físicas regularmente.",
+                afirmacao: [
+                    "Sinto que preciso melhorar meu nível de atividade física.",
+                    "Às vezes, tenho dificuldades para encontrar tempo ou motivação."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Você acredita que tem uma alimentação saudável?",
+        alternativas: [
+            {
+                texto: "Sim, faço escolhas alimentares saudáveis.",
+                afirmacao: [
+                    "Sinto que minha alimentação contribui para meu bem-estar.",
+                    "Procuro incluir frutas, vegetais e grãos integrais na minha dieta."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Não, minha alimentação poderia ser melhor.",
+                afirmacao: [
+                    "Reconheço que muitas vezes opto por alimentos menos saudáveis.",
+                    "Estou ciente de que preciso fazer mudanças na minha dieta."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Você se sente estressado com frequência?",
+        alternativas: [
+            {
+                texto: "Não, consigo lidar bem com o estresse.",
+                afirmacao: [
+                    "Utilizo técnicas de relaxamento e autocuidado.",
+                    "Acredito que meu equilíbrio emocional é bom."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Sim, frequentemente me sinto estressado.",
+                afirmacao: [
+                    "Sinto que o estresse afeta meu dia a dia.",
+                    "Estou buscando maneiras de gerenciar melhor o estresse."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Você costuma dormir o suficiente todas as noites?",
+        alternativas: [
+            {
+                texto: "Sim, durmo de 7 a 9 horas por noite.",
+                afirmacao: [
+                    "Acredito que o sono é fundamental para minha saúde.",
+                    "Me sinto bem descansado ao acordar."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Não, geralmente durmo menos do que deveria.",
+                afirmacao: [
+                    "Sinto que a falta de sono me afeta durante o dia.",
+                    "Estou tentando melhorar meus hábitos de sono."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Você acha importante reservar um tempo para autocuidado?",
+        alternativas: [
+            {
+                texto: "Sim, considero essencial cuidar de mim mesmo.",
+                afirmacao: [
+                    "Dedico tempo para atividades que me relaxam e me fazem feliz.",
+                    "Acredito que o autocuidado é fundamental para o bem-estar."
+                ],
+                proxima: 9,
+            },
+            {
+                texto: "Não, geralmente não tenho tempo para autocuidado.",
+                afirmacao: [
+                    "Sinto que as obrigações do dia a dia me impedem de cuidar de mim.",
+                    "Estou aprendendo a importância do autocuidado."
+                ],
+                proxima: 10,
+            },
+        ]
+    },
+];
